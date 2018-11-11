30f036b2717a397e95a0bba6e6f7ae7dbb33f528
“Batman” <javier.soto@raymondjames.com>
2018-11-11 17:44:57 -0500

    Fix: react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 //Improve Readability
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter><App /></BrowserRouter>,
+  document.getElementById('root')
+)
